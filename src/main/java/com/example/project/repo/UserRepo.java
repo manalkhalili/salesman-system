7319c02ab0d60e0391ccb79fd9a1c1725ed07d26
manal <khalilim387@gmail.com>
2025-01-28 17:32:17 +0200

    hi22

--- src/main/java/com/example/project/repo/UserRepo.java
+++ src/main/java/com/example/project/repo/UserRepo.java
@@ -1,4 +1,21 @@
 package com.example.project.repo;
+import com.example.project.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public class UserRepo {
+@Repository
+public interface UserRepo extends JpaRepository<UserInfo, Integer> {
+    boolean existsByEmail(String email);
+
+    List<UserInfo> findByName(String name);
+
+    UserInfo findByPhoneNumber(String phoneNumber);
+
+    UserInfo findByEmail(String email);
+
+
+    List<UserInfo> findByRole(UserInfo.Role role);
+
+   ;
 }
